da9b9f8987b9b4216516e55a9e70a6e7584b0b58
Cody Kilpatrick <2489375+Codykilpatrick@users.noreply.github.com>
2023-01-04 18:06:55 -0500

    add data file, router and view for skills

---
+++ data/skill-data.js
@@ -0,0 +1,9 @@
+const skills = [
+  {text: 'JavaScript', done: true, _id: 125223},
+  {text: 'HTML', done: true, _id: 127904},
+  {text: 'Express', done: false, _id: 139608},
+]
+
+export { 
+	skills
+}
