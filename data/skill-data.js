68db0b0384ad396cb0b5ae9b3850a346896bec7b
Cody Kilpatrick <2489375+Codykilpatrick@users.noreply.github.com>
2023-01-04 18:28:33 -0500

    complete dev skills lab part 2

--- data/skill-data.js
+++
@@ -1,9 +0,0 @@
-const skills = [
-  {text: 'JavaScript', done: true, _id: 125223},
-  {text: 'HTML', done: true, _id: 127904},
-  {text: 'Express', done: false, _id: 139608},
-]
-
-export { 
-	skills
-}
